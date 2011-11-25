6f5c4d06af382234a7679c329ca3124ec0cd5231
Benoit Moussaud <bmoussaud@xebialabs.com>
2011-11-25 13:52:34 +0100

    add a test to replace square brackets

--- src/test/java/com/xebialabs/deployit/cli/ext/mustachify/transform/RegexReplaceTransformerTest.java
+++ src/test/java/com/xebialabs/deployit/cli/ext/mustachify/transform/RegexReplaceTransformerTest.java
@@ -20,38 +20,42 @@
  */
 package com.xebialabs.deployit.cli.ext.mustachify.transform;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableMap;
 import com.xebialabs.deployit.cli.ext.mustachify.transform.RegexReplaceTransformer.RegexReplaceTransformerFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit tests for the {@link RegexReplaceTransformer}
  */
 public class RegexReplaceTransformerTest {
 
-    @Test
-    public void findsPattern() {
-        RegexReplaceTransformer transformer = get("^.*$", "long");
-        assertEquals("long", transformer.transform("A long .* string"));
-    }
-    
-    @Test
-    public void supportsMatchGroupReplacement() {
-        RegexReplaceTransformer transformer = get("(long)", "l$1g");
-        assertEquals("A llongg (llongg) string", transformer.transform("A long (long) string"));
+	@Test
+	public void findsPattern() {
+		RegexReplaceTransformer transformer = get("^.*$", "long");
+		assertEquals("long", transformer.transform("A long .* string"));
+	}
 
-    }
-    
-    private static RegexReplaceTransformer get(String patternToFind, String replacement) {
-        return new RegexReplaceTransformer(ImmutableMap.of(
-                "type", RegexReplaceTransformerFactory.TRANSFORMER_TYPE,
-                "ci.type", "Dummy",
-                "encoding", Charsets.UTF_8.toString(),
-                "pattern", patternToFind,
-                "replacement", replacement));
-    }
+	@Test
+	public void supportsMatchGroupReplacement() {
+		RegexReplaceTransformer transformer = get("(long)", "l$1g");
+		assertEquals("A llongg (llongg) string", transformer.transform("A long (long) string"));
+	}
+
+	@Test
+	public void supportsMatchGroupReplacementBrackets() {
+		RegexReplaceTransformer transformer = get("\\[([^\\]]+)\\]", "\\{\\{$1\\}\\}");
+		assertEquals("A {{DATX}} string", transformer.transform("A [DATX] string"));
+	}
+
+	private static RegexReplaceTransformer get(String patternToFind, String replacement) {
+		return new RegexReplaceTransformer(ImmutableMap.of(
+				"type", RegexReplaceTransformerFactory.TRANSFORMER_TYPE,
+				"ci.type", "Dummy",
+				"encoding", Charsets.UTF_8.toString(),
+				"pattern", patternToFind,
+				"replacement", replacement));
+	}
 }
